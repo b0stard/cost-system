401314c9008e7e7b601b17454d921e44a98314b0
b0stard <155761442+b0stard@users.noreply.github.com>
2024-08-08 20:43:01 +0300

    Add files via upload

---
+++ system/src/main/java/org/hgwards/Expense.java
@@ -0,0 +1,11 @@
+package org.hgwards;
+
+import java.sql.Date;
+
+public class Expense {
+    private int id;
+    private Date date;
+    private String category;
+    private double amount;
+    private String description;
+}
